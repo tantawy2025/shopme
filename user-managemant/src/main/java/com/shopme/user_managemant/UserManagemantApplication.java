843b8a230c9fb7b879bcd315993694cfb9596180
Mohamed Mamdouh Tantawy <mohamed.tantawy@DOMAIN.ITSOFT.COM>
2024-10-13 08:38:35 +0300

    user managment init

---
+++ user-managemant/src/main/java/com/shopme/user_managemant/UserManagemantApplication.java
@@ -0,0 +1,13 @@
+package com.shopme.user_managemant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagemantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagemantApplication.class, args);
+	}
+
+}
---
+++ user-managemant/src/test/java/com/shopme/user_managemant/UserManagemantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopme.user_managemant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagemantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
